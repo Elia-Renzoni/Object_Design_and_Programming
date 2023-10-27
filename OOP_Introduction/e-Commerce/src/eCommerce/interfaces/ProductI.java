53ae51bf99ccf9cc232fe0ab204e116f4580acf5
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2023-10-27 18:13:34 +0200

    Delete OOP_Introduction/e-Commerce directory

--- OOP_Introduction/e-Commerce/src/eCommerce/interfaces/ProductI.java
+++
@@ -1,7 +0,0 @@
-package eCommerce.interfaces;
-
-public interface ProductI {
-	void showProducts();
-	int searchProduct(int productId);
-	boolean payment(int productId);
-}
--- OOP_Introduction/e-Commerce/src/eCommerce/interfaces/ShoppingCart.java
+++
@@ -1,8 +0,0 @@
-package eCommerce.interfaces;
-
-public interface ShoppingCart {
-	void showShoppingCart();
-	boolean addProduct(int productId, String desc);
-	int searchProduct(int productId);
-	boolean remouveProducts(int productId);
-}
