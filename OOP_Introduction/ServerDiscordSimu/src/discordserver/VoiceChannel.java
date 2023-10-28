98b8a535f7dd75d483632daf0d7eb058bbffce0b
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2023-10-28 15:29:48 +0200

    Update VoiceChannel.java

--- OOP_Introduction/ServerDiscordSimu/src/discordserver/VoiceChannel.java
+++ OOP_Introduction/ServerDiscordSimu/src/discordserver/VoiceChannel.java
@@ -3,8 +3,28 @@ package discordserver;
 public class VoiceChannel extends TextChannel {
 	private String[] conntectedUsers;
 	
-	public VoiceChannel(int chId, String chName, int usId, String usName) {
+	public VoiceChannel(int chId, String chName, int usId, String usName, String[] objects) {
 		super(chId, chName, usId, usName);
-		// TODO Auto-generated constructor stub
-	}	
+		this.conntectedUsers = objects;
+	}
+	
+	public boolean connectNewUser(int userId, String userName) {
+		boolean tmp = false;
+		try {
+			conntectedUsers = new String[] {new String(userName)};
+			System.out.println("Nuovo utente collegato !"); 
+			System.out.println(super.user.toString());
+			tmp = true;
+		} catch (Exception ex) {
+			System.err.println(ex);
+		}
+		return tmp;
+	}
+
+	public void getConnectedUsers() {
+		for (var value : this.conntectedUsers) {
+			System.out.println(value);
+		}
+	}
+	
 }
