2f80fa8b59ea32a1836d5af9cadb8160d3e6db3b
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2023-10-28 16:59:42 +0200

    Update Client.java

--- OOP_Introduction/ServerDiscordSimu/src/discordserver/Client.java
+++ OOP_Introduction/ServerDiscordSimu/src/discordserver/Client.java
@@ -2,16 +2,16 @@ package discordserver;
 
 public class Client {
 	public static void main(String[] args) {
-        User user = new User(12, "Elia");
-        System.out.println(user.toString());
-        TextChannel txt = new TextChannel(33, "Coding", 12, "Elia");
-        String text = new String("Ciao a tutti");
-        txt.sendMessage(text);
-        Member mb = new Member(12, "Elia", "moderator");
-        text = "Attenzione !";
-        mb.sendMessage(text);
-        String[] obj = new String[] {new String("Giovanni"), new String("Franceso")};
-        VoiceChannel ch = new VoiceChannel(34, "Art", 23, "Giova", obj);
-        ch.getConnectedUsers();
-    }
+        	User user = new User(12, "Elia");
+        	System.out.println(user.toString());
+        	TextChannel txt = new TextChannel(33, "Coding", 12, "Elia");
+        	String text = new String("Ciao a tutti");
+        	txt.sendMessage(text);
+        	Member mb = new Member(12, "Elia", "moderator");
+        	text = "Attenzione !";
+        	mb.sendMessage(text);
+        	String[] obj = new String[] {new String("Giovanni"), new String("Franceso")};
+        	VoiceChannel ch = new VoiceChannel(34, "Art", 23, "Giova", obj);
+        	ch.getConnectedUsers();
+    	}
 }
