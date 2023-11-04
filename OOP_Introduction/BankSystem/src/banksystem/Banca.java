152f9d9c30490a512aac41901249682004a4f7f6
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2023-11-04 11:25:42 +0100

    Add files via upload

--- OOP_Introduction/BankSystem/src/banksystem/Banca.java
+++ OOP_Introduction/BankSystem/src/banksystem/Banca.java
@@ -1,5 +1,19 @@
 package banksystem;
 
 public class Banca {
-
+	private CartaDiCreditoStandard cartaDiCredito;
+	private CartaDiCreditoCashBack cartaCashBack;
+	private ContoCorrente contoCorrente;
+	
+	public Banca(int numero, String titolare, double limite, int numeroConto, String titolareConto, double saldo) {
+		this.cartaDiCredito = new CartaDiCreditoStandard(numero, titolare, limite, numeroConto, titolareConto, saldo);
+	}
+	
+	public Banca(int numero, String titolare, double limite, int numeroConto, String titolareConto, double saldo, boolean isCashBack) {
+		this.cartaCashBack = new CartaDiCreditoCashBack(numero, titolare, limite, numeroConto, titolareConto, saldo);
+	}
+	
+	public Banca(int numeroConto, String nomeTitolare, double saldo) {
+		this.contoCorrente = new ContoCorrente(numeroConto, nomeTitolare, saldo);
+	}
 }
