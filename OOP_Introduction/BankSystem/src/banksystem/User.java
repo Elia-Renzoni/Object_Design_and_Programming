611dcd5380a0334d744ae19b08486d04c130406a
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2023-11-04 13:45:38 +0100

    Update User.java

--- OOP_Introduction/BankSystem/src/banksystem/User.java
+++ OOP_Introduction/BankSystem/src/banksystem/User.java
@@ -2,6 +2,17 @@ package banksystem;
 
 public class User {
 	public static void main(String[] args) {
-		// TODO
+		// Simple Test
+		Banca banca = new Banca(12, "Giacomo", 3000.45);
+		banca.getConto().deposita(300);
+		banca.getConto().preleva(30);
+		System.out.println("Titolare Conto :" + banca.getConto().getTitolare());
+		
+		
+		Banca bb = new Banca(34, "Francesco", 5000.00, 56, "Francesco", 70000.00);
+		bb.getCarta().effettuaPagamento(500);
+		System.out.println("Titolare Carta : "  + bb.getCarta().getTitolareCarta());
+		System.out.println("Titolare del Conto :" + bb.getCarta().getContoCorrente().getTitolare());
+		
 	}
 }
