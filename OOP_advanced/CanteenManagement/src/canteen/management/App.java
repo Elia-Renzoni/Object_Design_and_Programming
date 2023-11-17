68abffd86eec3e31037eedfc6dd9f35333301b2b
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2023-11-17 11:34:46 +0100

    Add files via upload

--- OOP_advanced/CanteenManagement/src/canteen/management/App.java
+++ OOP_advanced/CanteenManagement/src/canteen/management/App.java
@@ -1,7 +1,20 @@
 package canteen.management;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class App {
 	public static void main(String[] args) {
+		List<String> ingredients = new LinkedList<String>();
+		ingredients.add(new String("Pasta"));
+		ingredients.add(new String("Melanzane"));
+		ingredients.add(new String("Pomodoro"));
 		
+		Dish d1 = new Dish("Pasta alla norma", 9.70, ingredients);
+		
+		Order<Dish> ord = new Order<>();
+		ord.addProductToOrder(d1);
+		System.out.println("Prezzo Totale : " + ord.engineTotalPrice());
+		System.out.println(ord.getOrder());
 	}
 }
