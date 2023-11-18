602094c9f9eebb3a38f2e97119fd7af6c0a0129b
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2023-11-18 09:59:54 +0100

    Add files via upload

--- OOP_advanced/IoTSystem/src/system/IoTDeviceManagement.java
+++ OOP_advanced/IoTSystem/src/system/IoTDeviceManagement.java
@@ -6,34 +6,12 @@ import java.util.Map;
 
 public class IoTDeviceManagement <Integer, E> {
 	private HashMap<Integer, E> iotDevices;
-	private Iterator<Map.Entry<Integer, E>> iterator;
 	
 	public IoTDeviceManagement() {
 		this.iotDevices = new HashMap<>();
-		this.iterator = this.iotDevices.entrySet().iterator();
 	}
 	
 	public void addElement(Integer deviceId, E device) {
 		this.iotDevices.put(deviceId, device);
-	}
-	
-	public void notifyDevices() {
-		while (this.iterator.hasNext()) {
-			Map.Entry<Integer, E> value = this.iterator.next();
-			E device = value.getValue();
-			if (device instanceof HumiditySensor) {
-				if(((HumiditySensor)device).isWarnable()) {
-					if (((TemperatureSensor)device).isWarnable())  {
-						System.out.println("Attenzione Regolare la Temperatura");
-					}
-				} 
-			} else if (device instanceof GasDetector) {
-				if(((GasDetector)device).isWarnable()) {
-					if (((TemperatureSensor)device).isWarnable())  {
-						System.out.println("Attenzione Regolare la Temperatura, GAS ALTO");
-					}
-				} 
-			}
-		}
 	}
 }
--- OOP_advanced/IoTSystem/src/system/UserApp.java
+++ OOP_advanced/IoTSystem/src/system/UserApp.java
@@ -1,5 +1,18 @@
 package system;
 
 public class UserApp {
-
+	public static void main(String[] args) {
+		HumiditySensor mySensor = new HumiditySensor();
+		GasDetector myGasDet = new GasDetector();
+		TemperatureSensor tempSens = new TemperatureSensor();
+		
+		if (!mySensor.increaseActualValue(12)) {
+			mySensor.addValue();
+		} else {
+			System.out.println("Attenzione");
+		}
+		
+		IoTDeviceManagement<Integer, HumiditySensor> man1 = new IoTDeviceManagement<>();
+		man1.addElement(23, mySensor);
+	}
 }
