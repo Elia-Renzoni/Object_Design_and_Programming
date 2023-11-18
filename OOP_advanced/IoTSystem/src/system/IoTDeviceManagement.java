b685ffca0cd60d0310b39c183d6407e8062e811b
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2023-11-18 08:45:41 +0100

    Add files via upload

--- OOP_advanced/IoTSystem/src/system/IoTDeviceManagement.java
+++ OOP_advanced/IoTSystem/src/system/IoTDeviceManagement.java
@@ -1,14 +1,39 @@
 package system;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
-public class IoTDeviceManagement <E> {
+public class IoTDeviceManagement <Integer, E> {
 	private HashMap<Integer, E> iotDevices;
+	private Iterator<Map.Entry<Integer, E>> iterator;
 	
 	public IoTDeviceManagement() {
 		this.iotDevices = new HashMap<>();
+		this.iterator = this.iotDevices.entrySet().iterator();
 	}
 	
-	// TODO add values
+	public void addElement(Integer deviceId, E device) {
+		this.iotDevices.put(deviceId, device);
+	}
 	// TODO notify all sensor if some sensor make important changes
+	public void notifyDevices() {
+		while (this.iterator.hasNext()) {
+			Map.Entry<Integer, E> value = this.iterator.next();
+			E device = value.getValue();
+			if (device instanceof HumiditySensor) {
+				if(((HumiditySensor)device).isWarnable()) {
+					if (((TemperatureSensor)device).isWarnable())  {
+						System.out.println("Attenzione Regolare la Temperatura");
+					}
+				} 
+			} else if (device instanceof GasDetector) {
+				if(((GasDetector)device).isWarnable()) {
+					if (((TemperatureSensor)device).isWarnable())  {
+						System.out.println("Attenzione Regolare la Temperatura, GAS ALTO");
+					}
+				} 
+			}
+		}
+	}
 }
