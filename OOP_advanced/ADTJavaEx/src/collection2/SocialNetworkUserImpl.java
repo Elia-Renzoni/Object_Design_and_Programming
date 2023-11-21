69879a47979b670f161fd29d60ea2f908bf194ef
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2023-11-21 17:35:48 +0100

    Add files via upload

---
+++ OOP_advanced/ADTJavaEx/src/collection2/SocialNetworkUserImpl.java
@@ -0,0 +1,114 @@
+
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map.Entry;
+
+/**
+ * 
+ * Instructions
+ * 
+ * This will be an implementation of
+ * {@link it.unibo.oop.lab.collections2.SocialNetworkUser}:
+ * 
+ * 1) complete the definition of the methods by following the suggestions
+ * included in the comments below.
+ * 
+ * @param <U>
+ *            Specific user type
+ */
+public class SocialNetworkUserImpl<U extends User> extends UserImpl implements SocialNetworkUser<U> {
+
+    /*
+     * 
+     * [FIELDS]
+     * 
+     * Define any necessary field
+     * 
+     * In order to save the people followed by a user organized in groups, adopt
+     * a generic-type Map:
+     * 
+     * think of what type of keys and values would best suit the requirements
+     */
+    
+    private HashMap<String, LinkedList<U>> followers;
+    private Iterator<Entry<String, LinkedList<U>>> it = this.followers.entrySet().iterator();
+
+    /*
+     * [CONSTRUCTORS]
+     * 
+     * 1) Complete the definition of the constructor below, for building a user
+     * participating in a social network, with 4 parameters, initializing:
+     * 
+     * - firstName - lastName - username - age and every other necessary field
+     * 
+     * 2) Define a further constructor where age is defaulted to -1
+     */
+
+    /**
+     * Builds a new {@link SocialNetworkUserImpl}.
+     * 
+     * @param name
+     *            the user firstname
+     * @param surname
+     *            the user lastname
+     * @param userAge
+     *            user's age
+     * @param user
+     *            alias of the user, i.e. the way a user is identified on an
+     *            application
+     */
+    public SocialNetworkUserImpl(final String name, final String surname, final String user, final int userAge) {
+        super(name, surname, user, userAge);
+        this.followers = new HashMap<>();
+    }
+
+    public SocialNetworkUserImpl(final String name, final String surname, final String user) {
+           this(name, surname, user, -1);     
+    }
+
+    /*
+     * [METHODS]
+     * 
+     * Implements the methods below
+     */
+
+    @Override
+    public boolean addFollowedUser(final String circle, final U user) {
+        if (!this.followers.containsValue(user)) {
+            this.followers.put(circle, new LinkedList<U>().add(user));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public Collection<U> getFollowedUsersInGroup(final String groupName) {
+        Collection<U> usersByGroupName;
+        while (it.hasNext()) {
+            Map.Entry<String, LinkedList<U>> mappa = (Map.Entry<String, LinkedList<U>>)it.next();
+            if (!(mappa.getKey() != groupName)) {
+                usersByGroupName = mappa.getValue();
+            }
+        }
+        return usersByGroupName;
+    }
+
+    @Override
+    public List<U> getFollowedUsers() {
+        List<U> generalUsers;
+        while (it.hasNext()) {
+            Map.Entry<String, LinkedList<U>> mappa = (Map.Entry<String, LinkedList<U>>)it.next();
+            generalUsers = mappa.getValue();
+        }
+        return generalUsers;
+    }
+
+}
