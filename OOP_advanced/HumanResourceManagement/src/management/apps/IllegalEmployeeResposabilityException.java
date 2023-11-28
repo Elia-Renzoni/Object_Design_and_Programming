715b9da3965150ece985211526866dabaffc7f7e
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2023-11-28 18:13:44 +0100

    Update IllegalEmployeeResposabilityException.java

--- OOP_advanced/HumanResourceManagement/src/management/apps/IllegalEmployeeResposabilityException.java
+++ OOP_advanced/HumanResourceManagement/src/management/apps/IllegalEmployeeResposabilityException.java
@@ -5,10 +5,8 @@ public class IllegalEmployeeResposabilityException extends Exception {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private String message;
 	
 	public IllegalEmployeeResposabilityException() {
-		super();
-		this.message = "Ruoli non giusti";
+		super("Ruoli non giusti");
 	}
 }
