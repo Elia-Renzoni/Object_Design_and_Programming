67da253acdc9445ed7c40c80c7bd2e323394fc49
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2024-01-03 11:09:15 +0100

    Update Stream2.java

--- OOP_advanced/Streams/Stream2.java
+++ OOP_advanced/Streams/Stream2.java
@@ -14,7 +14,7 @@ public class Stream2 {
         final List<Integer> listToUse = Arrays.asList(12, 34, 55, 2, 6, 3, 23, 57, 7);
         
         final Stream<Integer> listAfterStream = listToUse.stream()
-            .filter(n -> n % 3 == 0)
+            .filter(n -> n % 2 != 0)
             .map(n -> n * 2)
             .distinct()
             .sorted((n1, n2) -> n2 - n1)
