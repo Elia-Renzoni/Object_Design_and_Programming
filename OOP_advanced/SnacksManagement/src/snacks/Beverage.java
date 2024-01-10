85d5766cb831336b0a43c50d51c78cfbd7b95434
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2024-01-10 09:35:01 +0100

    Create Beverage.java

---
+++ OOP_advanced/SnacksManagement/src/snacks/Beverage.java
@@ -0,0 +1,30 @@
+
+public class Beverage implements Snack {
+    private final int snackId;
+    private final String snackName;
+    private final String snackProducer;
+    private final double snackPrice;
+
+    public Beverage(final String snackName, final String snackProducer, final double snackPrice) {
+        this.snackId = new Random().nextInt();
+        this.snackName = snackName;
+        this.snackProducer = snackProducer;
+        this.snackPrice = snackPrice;
+    }
+
+    public int getSnackId() {
+        return this.snackId;
+    }
+
+    public String getSnackName() {
+        return this.snackName;
+    }
+
+    public double getSanckPrice() {
+        return this.snackPrice;
+    }
+
+    public String getSnackProducer() {
+        return this.snackProducer;
+    }
+}
